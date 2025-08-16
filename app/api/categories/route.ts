19e865b17684ff2a912901925b2d36dacd827964
Tanujeet <singhtanujeet@gmail.com>
2025-08-16 09:52:52 +0530

    Added:categories get api dopne

---
+++ app/api/categories/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorised", { status: 403 });
+  }
+
+  try {
+    const findCategory = await prisma.category.findMany({
+      where: { userId },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json(findCategory);
+  } catch (e) {
+    console.error("Failed to fetch expense", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorised", { status: 403 });
+  }
+
+  try {
+  } catch (e) {
+    console.error("Failed to fetch expense", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
