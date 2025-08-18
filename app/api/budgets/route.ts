2bc8dc18289a4799befba781a2e54846602b5392
Tanujeet <singhtanujeet@gmail.com>
2025-08-18 08:33:53 +0530

    budget api POST done

--- app/api/budgets/route.ts
+++ app/api/budgets/route.ts
@@ -1,27 +1,40 @@
+import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-    const { userId } = await auth()
-    if (!userId) {
-        return new NextResponse("Unauthorised",{status:403})
-    }
-    try {
-        
-    } catch (e) {
-        console.error("Failed to get budget", e)
-        return new NextResponse("Internal Server Error",{status:500})        
-    }
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorised", { status: 403 });
+  }
+  try {
+  } catch (e) {
+    console.error("Failed to get budget", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
+
 export async function POST(req: Request) {
-    const { userId } = await auth()
-    if (!userId) {
-        return new NextResponse("Unauthorised",{status:403})
-    }
-    try {
-        
-    } catch (e) {
-        console.error("Failed to create budget", e)
-        return new NextResponse("Internal Server Error",{status:500})        
-    }
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+
+  const { limit, endDate, startDate, categoryId } = await req.json();
+  try {
+    const newBudget = await prisma.budget.create({
+      data: {
+        userId: userId,
+        categoryId: categoryId,
+        limit: limit,
+        startDate: startDate,
+        endDate: endDate,
+      },
+    });
+
+    return NextResponse.json(newBudget);
+  } catch (e) {
+    console.error("Failed to create budget", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
