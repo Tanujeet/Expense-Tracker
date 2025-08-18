b187aca88f92fccc9428d30769b36132a401ee2e
Tanujeet <singhtanujeet@gmail.com>
2025-08-18 08:43:47 +0530

    budget api GET  done

--- app/api/budgets/route.ts
+++ app/api/budgets/route.ts
@@ -1,18 +1,45 @@
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { get } from "http";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const { userId } = await auth();
   if (!userId) {
-    return new NextResponse("Unauthorised", { status: 403 });
+    return new NextResponse("Unauthorized", { status: 403 });
   }
   try {
+    const getBudget = await prisma.budget.findMany({
+      where: { userId: userId },
+      include: { category: true },
+    });
+
+    for (const budget of getBudget) {
+      const expenses = await prisma.expense.aggregate({
+        _sum: { amount: true },
+        where: {
+          userId,
+          categoryId: budget.categoryId, // yaha error nahi aayega ab
+          date: {
+            gte: budget.startDate,
+            lte: budget.endDate,
+          },
+        },
+      });
+
+      console.log(
+        `Category: ${budget.category.name}, Remaining: ${
+          budget.limit - (expenses._sum.amount ?? 0)
+        }`
+      );
+    }
   } catch (e) {
     console.error("Failed to get budget", e);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
+
+
 
 export async function POST(req: Request) {
   const { userId } = await auth();
