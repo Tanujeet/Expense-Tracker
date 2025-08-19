66184cf5135d71d214490c2dc3b8a79643c6fcfc
Tanujeet <singhtanujeet@gmail.com>
2025-08-19 09:24:28 +0530

    Added:RecurringBudget[id] DELETE logic

--- app/api/recurring/[id]/route.ts
+++ app/api/recurring/[id]/route.ts
@@ -1,37 +1,32 @@
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { detectConflictingPaths } from "next/dist/build/utils";
 import { NextResponse } from "next/server";
 
 export async function GET(
-  req: Request,{ params: paramsPromise }: { params: Promise<{ id: string }> }
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
 ) {
-    const { userId } = await auth()
-    if (!userId) {
-        return new NextResponse("Unauthorized",{status:403})
-    }
-    const { id } = await paramsPromise;
-    
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
 
-    try {
-      const expense = await prisma.recurringExpense.findUnique({
-            where:{id}
-      })
-        
-        if (!expense) {
-            return new NextResponse("Expense not found",{status:404})
-        }
-        if (!expense || expense.userId !== userId) {
-          return new NextResponse("Expense not found", { status: 404 });
-        }
-        return NextResponse.json(expense)
-    } catch (e) {
-        console.error("Failed to get recurring budget", e)
-        return new NextResponse("Internal Server Error",{status:500})
-        
+  try {
+    const expense = await prisma.recurringExpense.findUnique({
+      where: { id },
+    });
+
+    if (!expense || expense.userId !== userId) {
+      return new NextResponse("Expense not found", { status: 404 });
     }
+    return NextResponse.json(expense);
+  } catch (e) {
+    console.error("Failed to get recurring budget", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
-
-
 
 export async function PATCH(
   req: Request,
@@ -44,15 +39,11 @@ export async function PATCH(
   const { id } = await paramsPromise;
 
   try {
-    
   } catch (e) {
     console.error("Failed to Update budget", e);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
-
-
-
 
 export async function DELETE(
   req: Request,
@@ -65,7 +56,19 @@ export async function DELETE(
   const { id } = await paramsPromise;
 
   try {
-    
+    const expense = await prisma.recurringExpense.findUnique({
+      where: { id },
+    });
+
+    if (!expense || expense.userId !== userId) {
+      return new NextResponse("Expense not found", { status: 404 });
+    }
+
+    const delelteRecurringExpense = await prisma.recurringExpense.delete({
+      where: { id },
+    });
+
+    return NextResponse.json(delelteRecurringExpense);
   } catch (e) {
     console.error("Failed to Update budget", e);
     return new NextResponse("Internal Server Error", { status: 500 });
