05ef83847f7cc23ead320775cc6fc5ad0b3db319
Tanujeet <singhtanujeet@gmail.com>
2025-08-19 09:22:07 +0530

    Added:RecurringBudget[id] GET  logic

---
+++ app/api/recurring/[id]/route.ts
@@ -0,0 +1,76 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,{ params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+    const { userId } = await auth()
+    if (!userId) {
+        return new NextResponse("Unauthorized",{status:403})
+    }
+    const { id } = await paramsPromise;
+    
+
+    try {
+      const expense = await prisma.recurringExpense.findUnique({
+            where:{id}
+      })
+        
+        if (!expense) {
+            return new NextResponse("Expense not found",{status:404})
+        }
+        if (!expense || expense.userId !== userId) {
+          return new NextResponse("Expense not found", { status: 404 });
+        }
+        return NextResponse.json(expense)
+    } catch (e) {
+        console.error("Failed to get recurring budget", e)
+        return new NextResponse("Internal Server Error",{status:500})
+        
+    }
+}
+
+
+
+export async function PATCH(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
+
+  try {
+    
+  } catch (e) {
+    console.error("Failed to Update budget", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+
+
+
+export async function DELETE(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
+
+  try {
+    
+  } catch (e) {
+    console.error("Failed to Update budget", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+
+
