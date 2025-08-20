42cd1aff171c9c521faa74cb1db6fbea98fa88e6
Tanujeet <singhtanujeet@gmail.com>
2025-08-20 08:34:02 +0530

    Clerk and sign-in page done

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Page = async() => {
+    const { userId } = await auth()
+    if (userId) {
+        redirect("/dashboard")
+    }
+  return (
+    <main className="flex justify-center items-center h-screen"><SignIn redirectUrl="/dashboard"/></main>
+  );
+};
+
+export default Page;
