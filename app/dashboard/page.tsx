b6ffbc4cffbbef08601fd824ea5404586e925792
Tanujeet <singhtanujeet@gmail.com>
2025-08-25 08:11:29 +0530

    added chartjs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from "recharts";
 
 const Page = () => {
   const data = [
@@ -6,6 +9,14 @@ const Page = () => {
     { heading: "Total budget", amount: 25000 },
     { heading: "Remaining balance", amount: 20000 },
   ];
+  const datas = [
+    { name: "Food", value: 400 },
+    { name: "Rent", value: 300 },
+    { name: "Shopping", value: 300 },
+    { name: "Transport", value: 200 },
+    { name: "Entertainment", value: 100 },
+  ];
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#A78BFA"];
   return (
     <main>
       <section>
@@ -25,7 +36,7 @@ const Page = () => {
         </div>
       </section>
       <section className="mt-10">
-        <div className="flex">
+        <div className="flex gap-7">
           <Card className="w-[500px]">
             <CardHeader>
               <CardTitle className="font-bold text-2xl">
@@ -35,6 +46,37 @@ const Page = () => {
             <CardContent>
               <p>Card Content</p>
             </CardContent>
+          </Card>
+          <Card className="w-[400px]">
+            <div className="h-64 bg-white rounded-2xl p-3">
+              <h2 className="text-2xl font-bold">Spending by Category</h2>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={datas} // ✅ chart ka sahi dataset
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={90}
+                    paddingAngle={5}
+                    dataKey="value"
+                  >
+                    {datas.map(
+                      (
+                        entry,
+                        index // ✅ yahan bhi datas.map
+                      ) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={COLORS[index % COLORS.length]}
+                        />
+                      )
+                    )}
+                  </Pie>
+                  <Tooltip />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
           </Card>
         </div>
       </section>
