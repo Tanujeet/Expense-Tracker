b77adecc0969ffbb5b796ee18775ecc472c409da
Tanujeet <singhtanujeet@gmail.com>
2025-08-28 09:26:42 +0530

    Added :Table and card in expense page

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -8,14 +8,54 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 const Page = () => {
   return (
     <main>
       <section>
-        <div className="mt-10 ml-7 flex justify-between">
+        <div className="mt-10 ml-7 flex justify-between p-3">
           <h1 className="text-3xl font-bold">Expenses</h1>
-          <Button>Add New Expense</Button>
+          <Button>New Expense</Button>
+        </div>
+      </section>
+      <section>
+        <div className="p-10">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Date</TableHead>
+                <TableHead>Category</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+        <div className="mt-10">
+          <Card>
+            <CardHeader>
+              <CardTitle>This Month</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>20,000</p>
+            </CardContent>
+          </Card>
         </div>
       </section>
     </main>
