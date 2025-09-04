fa3f694e24815ebf5b3039e792539ef368a7c388
Tanujeet <singhtanujeet@gmail.com>
2025-09-04 08:20:01 +0530

    Added:Changes in summary route

--- app/api/summary/route.ts
+++ app/api/summary/route.ts
@@ -26,9 +26,20 @@ export async function GET() {
       },
     });
 
+    // 🔹 Budget
+    const totalBudget = await prisma.budget.aggregate({
+      _sum: { limit: true },
+      where: { userId },
+    });
+
+    const spent = totalSpentThisMonth._sum.amount || 0; // ✅ define spent
+    const budget = totalBudget._sum.limit || 0;
+
     return NextResponse.json({
-      thisMonth: totalSpentThisMonth._sum.amount || 0,
-      Totaltransactions: totalTransactions,
+      totalSpentThisMonth: spent,
+      totalBudget: budget,
+      remainingBalance: budget - spent,
+      totalTransactions,
     });
   } catch (e) {
     console.error("Failed to get summary", e);
